85b0e42540b72f8201e7f94bef8bdaaae2d6de49
Luis Alberto Pérez <laperezmu@gmail.com>
2021-10-25 13:01:52 -0500

    Added serializers

---
+++ movie_app/api/serializers.py
@@ -0,0 +1,25 @@
+from rest_framework import serializers
+from rest_framework.relations import StringRelatedField
+from movie_app.models import StreamPlatform, VisualContent, Review
+
+
+class ReviewSerializer(serializers.ModelSerializer):
+    review_user = serializers.StringRelatedField(read_only=True)
+
+    class Meta:
+        model = Review
+        fields = '__all__'
+
+class VisualContentSerializer(serializers.ModelSerializer)
+    platform = serializers.CharField(source = 'platform.name')
+
+    class meta:
+        model = VisualContent
+        fields = '__all__'
+
+class StreamPlatformSerializer(serializers.ModelSerializer)
+    visual_content = VisualContentSerializer(many = True, read_only = True)
+
+    class Meta:
+        model = StreamPlatform
+        fields = '__all__'
